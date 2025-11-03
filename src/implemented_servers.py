99d5989234933f3a7e5822de191511105a40c2b9
stephan-carstens <carstensstephan@outlook.com>
2025-11-03 13:18:05 +0200

    add goodwe GT register definitions

--- src/implemented_servers.py
+++ src/implemented_servers.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+from .goodwe_gt import GoodweGT
 from .goodwe_logger import GoodweLogger
 
 
 class ServerTypes(Enum):
     GOODWE_LOGGER = GoodweLogger
+    GOODWE_GT = GoodweGT
